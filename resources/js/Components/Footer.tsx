cd9740ddd3ceb193139d1323b9cab9cbe975e5e3
Thomas Andre Karlsen <contact@theunknown.no>
2021-06-18 16:00:01 +0200

    Apply suggestions from code review

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -6,17 +6,17 @@ const Footer = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-row justify-between text-gray-400 text-opacity-80 py-6">
                     <div className="flex-1">
-                        <p>{"NTec Media AS"}</p>
+                        <p>{"Ntec Media AS"}</p>
                         <p>Divvun</p>
                     </div>
                     <div className="flex-1">
                         <p className="flex-1">
-                            {"© NTec Media AS " + new Date().getFullYear()}
+                            {"© Ntec Media AS " + new Date().getFullYear()}
                         </p>
                         <p>Cookie Policy</p>
                     </div>
                     <p className="flex-1">
-                        Julev er en ordbok som er utviklet av NTech Media AS i
+                        Julev er en ordbok som er utviklet av Ntec Media AS i
                         samarbeid med UiT og med støtte fra sametinget.
                     </p>
                 </div>
