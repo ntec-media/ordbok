f98e3042efbd58fc0ad3ef941d9a97a48eda14c7
Patrick Lønhaug <patrlo@hotmail.com>
2021-06-23 17:45:23 +0200

    Interface and request added

---
+++ app/Http/Controllers/ISearch.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Requests\SearchRequest;
+
+interface ISearch {
+
+    public function words(SearchRequest $request);
+
+    public function lookup(SearchRequest $request);
+
+}
