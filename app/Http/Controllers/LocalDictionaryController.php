1c79d8abe32c3c64e113a3c9119e83c03b21a22c
Patrick Lønhaug <patrlo@hotmail.com>
2021-06-23 19:39:18 +0200

    format

--- app/Http/Controllers/LocalDictionaryController.php
+++ app/Http/Controllers/LocalDictionaryController.php
@@ -2,27 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\ISearch;
-use Illuminate\Support\Facades\DB;
 use App\Http\Requests\SearchRequest;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class LocalDictionaryController extends Controller implements ISearch
 {
-    public function words(SearchRequest $request) {
-        
+    public function words(SearchRequest $request)
+    {
         $data = json_decode($request->getContent());
 
         $d1 = DB::table('ord_norsk_samisk_BACKUP')->where('fra', 'like', '%' . $data->value . '%')->get();
         $d2 = DB::table('ord_norsk_samisk_BACKUP')->where('til', 'like', '%' . $data->value . '%')->get();
-        
+
         return Inertia::render('/', [
-            $d1->merge($d2)
+            $d1->merge($d2),
         ]);
     }
 
-    public function lookup(SearchRequest $request) {
-
+    public function lookup(SearchRequest $request)
+    {
     }
 }
