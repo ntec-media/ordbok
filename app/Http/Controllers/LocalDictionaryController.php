d9d9a83ad2f078584d9cff849a26be443e362edd
Patrick Lønhaug <patrlo@hotmail.com>
2021-06-23 19:54:58 +0200

    Changed url to component

--- app/Http/Controllers/LocalDictionaryController.php
+++ app/Http/Controllers/LocalDictionaryController.php
@@ -2,25 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SearchRequest;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ISearch;
 use Illuminate\Support\Facades\DB;
+use App\Http\Requests\SearchRequest;
 use Inertia\Inertia;
 
 class LocalDictionaryController extends Controller implements ISearch
 {
-    public function words(SearchRequest $request)
-    {
+    public function words(SearchRequest $request) {
+        
         $data = json_decode($request->getContent());
 
         $d1 = DB::table('ord_norsk_samisk_BACKUP')->where('fra', 'like', '%' . $data->value . '%')->get();
         $d2 = DB::table('ord_norsk_samisk_BACKUP')->where('til', 'like', '%' . $data->value . '%')->get();
-
-        return Inertia::render('/', [
-            $d1->merge($d2),
+        
+        return Inertia::render('Search', [
+            'data' => $d1->merge($d2)
         ]);
     }
 
-    public function lookup(SearchRequest $request)
-    {
+    public function lookup(SearchRequest $request) {
+
     }
 }
+
