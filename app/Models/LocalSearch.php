d5233a4ec12d6800cacafe3c98e852147ef1d1a3
Thomas Andre Karlsen <contact@theunknown.no>
2021-09-08 15:56:29 +0200

    Optimize search query

--- app/Models/LocalSearch.php
+++ app/Models/LocalSearch.php
@@ -21,60 +21,19 @@ class LocalSearch implements SearchInterface
             return $currentPage;
         });
 
-        if ($currentPage == 1) {
-            $query1 = DB::table('ord_norsk_samisk_BACKUP')
-            ->where('fra', $request->input('search'))
-            ->whereIn('kredittering', $request->input('dicts'))
-            ->get();
+        $search = preg_replace('/((?<=[^*])( ))+/', '* ', $request->input('search'));
 
-            $query4 = DB::table('ord_norsk_samisk_BACKUP')
-            ->where('til', $request->input('search'))
-            ->whereIn('kredittering', $request->input('dicts'))
-            ->get();
-        }
+        $search = str_ends_with($search, '*') ? $search : $search . '*';
 
-        $query2 = DB::table('ord_norsk_samisk_BACKUP')
-        ->where('fra', 'like', '%' . $request->input('search'))
-        ->whereIn('kredittering', $request->input('dicts'))
-        ->paginate(25, ['*']);
+        $results = DB::table('ord_norsk_samisk_BACKUP')
+                    ->select('*')
+                    ->whereIn('kredittering', $request->input('dicts'))
+                    ->whereRaw("MATCH (fra,til) AGAINST (? IN BOOLEAN MODE)", $search)
+                    ->orderByRaw("MATCH (fra) AGAINST (? IN BOOLEAN MODE) DESC", $search)
+                    ->orderByRaw("MATCH (til) AGAINST (? IN BOOLEAN MODE) DESC", $search);
 
-        $query3 = DB::table('ord_norsk_samisk_BACKUP')
-        ->where('fra', 'like', $request->input('search') . '%')
-        ->whereIn('kredittering', $request->input('dicts'))
-        ->paginate(25, ['*']);
-
-        $query5 = DB::table('ord_norsk_samisk_BACKUP')
-        ->where('til', 'like', '%' . $request->input('search'))
-        ->whereIn('kredittering', $request->input('dicts'))
-        ->paginate(25, ['*']);
-
-        $query6 = DB::table('ord_norsk_samisk_BACKUP')
-        ->where('til', 'like', $request->input('search') . '%')
-        ->whereIn('kredittering', $request->input('dicts'))
-        ->paginate(25, ['*']);
-
-        $query7 = DB::table('ord_norsk_samisk_BACKUP')
-        ->where('fra', 'like', '%' . $request->input('search') . '%')
-        ->whereIn('kredittering', $request->input('dicts'))
-        ->paginate(10, ['*']);
-
-        $query8 = DB::table('ord_norsk_samisk_BACKUP')
-        ->where('til', 'like', '%' . $request->input('search') . '%')
-        ->whereIn('kredittering', $request->input('dicts'))
-        ->paginate(10, ['*']);
-
-        if ($currentPage == 1) {
-            return $query1
-            ->merge($query2->items())
-            ->merge($query3->items())
-            ->merge($query4)
-            ->merge($query5->items())
-            ->merge($query6->items())
-            ->merge($query7->items())
-            ->merge($query8->items());
-        } else {
-            return array_merge($query2->items(), $query3->items(),  $query5->items(), $query6->items(), $query7->items(), $query8->items());
-        }
+        return $results->paginate(25)->items();
+        ;
     }
 
     /**
