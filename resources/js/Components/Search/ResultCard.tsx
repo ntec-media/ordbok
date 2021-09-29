e29def50c0e38b92829d28fa2d7e862b5b46efbd
Patrick Lønhaug <patrlo@hotmail.com>
2021-09-29 17:52:57 +0200

    js updated?

--- resources/js/Components/Search/ResultCard.tsx
+++ resources/js/Components/Search/ResultCard.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 const ResultCard = (props: Props) => {
-    function getHighlightedText(text: string) {
+    function getTextWithHgihtligh(text: string) {
         // Split text on highlight term, include term itself into parts, ignore case
         const parts = text.split(new RegExp(`(${props.input})`, 'gi'));
         return (
@@ -26,15 +26,15 @@ const ResultCard = (props: Props) => {
     return (
         <div className="flex-col w-full p-4 border border-indigo-200 rounded-lg shadow-md bg-gradient-to-r from-gray-100 via-gray-50 to-gray-100">
             <div className="flex justify-between w-full mb-6">
-                <p className="font-bold">
-                    {getHighlightedText(props.result.fra)}
+                <p className="font-semibold">
+                    {getTextWithHgihtligh(props.result.fra)}
                 </p>
                 <p className="italic font-bold text-blue-600">
                     {props.result.kredittering}
                 </p>
             </div>
             <div className="flex justify-between w-full mb-6">
-                {getHighlightedText(props.result.til)}
+                {getTextWithHgihtligh(props.result.til)}
             </div>
         </div>
     );
