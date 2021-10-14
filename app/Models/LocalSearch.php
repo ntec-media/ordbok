88ca5e9f55f1b974fa88421de1c13b1f5099eaac
Patrick Lønhaug <patrlo@hotmail.com>
2021-10-14 12:59:57 +0200

    Added dictionaries to the search query

--- app/Models/LocalSearch.php
+++ app/Models/LocalSearch.php
@@ -18,44 +18,54 @@ class LocalSearch implements SearchInterface
     {
         $search = $request->input('search');
         $orderBy = $request->input('orderBy') === "sami" ? "DESC" : "ASC";
+        $dictionaries = ["A. Kintel 2013", "Sáme Giellagálldo 2013", "Medisijnalasj báhkogirjje"];
+
 
         $query1 = DB::table('smj_translations')
             ->select()
-            ->where("til", "LIKE", "%{$search}%");
+            ->where("til", "LIKE", "%{$search}%")
+            ->whereIn('kredittering', $dictionaries);
 
         $query2 = DB::table('smj_translations')
             ->select()
             ->where("til", "LIKE", "%{$search}")
+            ->whereIn('kredittering', $dictionaries)
             ->union($query1);
 
         $query3 = DB::table('smj_translations')
             ->select()
             ->where("til", "LIKE", "{$search}%")
+            ->whereIn('kredittering', $dictionaries)
             ->union($query2);
 
         $query4 = DB::table('smj_translations')
             ->select()
             ->where("til", "LIKE", "{$search}")
+            ->whereIn('kredittering', $dictionaries)
             ->union($query3);
 
         $query5 = DB::table('smj_translations')
             ->select()
             ->where("fra", "LIKE", "%{$search}%")
+            ->whereIn('kredittering', $dictionaries)
             ->union($query4);
 
         $query6 = DB::table('smj_translations')
             ->select()
             ->where("fra", "LIKE", "%{$search}")
+            ->whereIn('kredittering', $dictionaries)
             ->union($query5);
 
         $query7 = DB::table('smj_translations')
             ->select()
             ->where("fra", "LIKE", "{$search}%")
+            ->whereIn('kredittering', $dictionaries)
             ->union($query6);
 
         $query8 = DB::table('smj_translations')
             ->select()
             ->where("fra", "LIKE", "{$search}")
+            ->whereIn('kredittering', $dictionaries)
             ->union($query7)
             ->limit(50)
             ->orderBy('oversatt_fra', $orderBy, 'oversatt_til', $orderBy)
