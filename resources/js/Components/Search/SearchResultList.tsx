57636723fbec24a509ba7534991c454b74af54a1
Patrick Lønhaug <patrlo@hotmail.com>
2021-10-27 14:33:38 +0200

    Added basic error-message if result object is not in response

--- resources/js/Components/Search/SearchResultList.tsx
+++ resources/js/Components/Search/SearchResultList.tsx
@@ -1,7 +1,8 @@
 import {Button, CircularProgress, List, ListItem} from '@material-ui/core';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import ISearchResult from '../../Interfaces/ISearchResult';
 import {search} from '../../utils';
+import CustomSnackbar, {CustomSnackbarProps} from '../Shared/CustomSnackbar';
 import ResultCard from './ResultCard';
 
 interface Props {
@@ -13,6 +14,13 @@ const SearchResultList = (props: Props) => {
     const [results, setResults] = useState<ISearchResult[]>([]);
     const [loading, setLoading] = useState(false);
     const [page, setPage] = useState<number>(1);
+    const mountedRef = useRef(true);
+    const [snackbarProps, setSnackbarProps] = useState<CustomSnackbarProps>({
+        type: 'error',
+        open: false,
+        message: 'En feil på serveren har oppstått',
+        handleClose: () => setSnackbarProps({...snackbarProps, open: false}),
+    });
 
     useEffect(() => {
         setPage(1);
@@ -21,14 +29,23 @@ const SearchResultList = (props: Props) => {
         } else setResults([]);
     }, [props.input, props.orderBy]);
 
+    useEffect(() => {
+        return () => {
+            mountedRef.current = false;
+        };
+    }, []);
+
     const getResultsArray = async (searchPage: number) => {
         setLoading(true);
         search(props.input, searchPage, props.orderBy)
             .then(res => {
-                if (results.length > 0 && searchPage > 1) {
+                if (!mountedRef.current) return null;
+                if (results?.length > 0 && searchPage > 1) {
                     setResults(results.concat(res.data));
                 } else {
-                    setResults(res.data);
+                    res.data
+                        ? setResults(res.data)
+                        : setSnackbarProps({...snackbarProps, open: true});
                     // Move searchfield to top of page but also steals focus which will be more frustrating
                     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                     // @ts-ignore
@@ -44,7 +61,7 @@ const SearchResultList = (props: Props) => {
 
     return (
         <>
-            {results.length > 0 ? (
+            {results?.length > 0 ? (
                 <>
                     {loading ? (
                         <div className="text-center">
@@ -89,6 +106,7 @@ const SearchResultList = (props: Props) => {
             ) : (
                 <h2 className="text-center">Fant Ingen resultat</h2>
             )}
+            <CustomSnackbar {...snackbarProps} />
         </>
     );
 };
