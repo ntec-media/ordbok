96dd0d70c1f0db2bfedc5e7f39bd7b6dfa9136da
Thomas Andre Karlsen <contact@theunknown.no>
2021-11-17 19:00:55 +0100

    Add legacy version view

---
+++ resources/views/legacy.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <title>Julevbágo</title>
+  </head>
+  <body>
+    <div class="absolute inset-0 flex items-center justify-center">
+        Utdatert versjon
+    </div>
+  </body>
+</html>
